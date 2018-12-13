020e68e22565e94ef275e0ddcdf155f735b92283
jusx <hello@justinlaw.org>
2018-12-13 01:56:46 -0800

    By default make ls to always execute with -Fa

--- dots/profile.sh
+++ dots/profile.sh
@@ -6,4 +6,17 @@ source ~/.dots/prompt.sh
 
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
-alias ls='ls -GFh'
+
+
+ls_ext() {
+  if [ $# -eq 0 ]; then # if ls is called without any arguments
+    # make ls print full info, with color
+    ls -Fa
+  elif [ $1='-la' ]
+  then
+    ls -GFhla
+  else
+    ls $@ # otherwise call as intended
+  fi
+}
+alias ls="ls_ext"
